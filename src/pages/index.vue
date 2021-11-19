fb4ba48d123bbbf7245b86704fec61a0743064e8
1Mill <erik.ekberg@outlook.com>
2021-11-18 21:11:03 -0800

    Scaffold cloudevent outputs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,43 @@
+<script>
+import { Cloudevent } from '@1mill/cloudevents'
+
+export default {
+	data: () => ({
+		data: null,
+		source: 'build.cloudevents.gdn',
+		type: 'cmd.placeholder.v0',
+	}),
+	computed: {
+		cloudevent() {
+			if (!this.source) return "Source is required"
+			if (!this.type) return "Type is required"
+
+			const attributes = {
+				source: this.source,
+				type: this.type,
+			}
+			if (this.data) attributes.data = JSON.stringify(this.data)
+
+			const cloudevent = new Cloudevent(attributes)
+			return cloudevent
+		},
+	},
+}
+</script>
 <template>
-	<h1>Hello world!</h1>
+	<main>
+		<article>
+			<header>
+				<h1>Cloudevent Builder</h1>
+				<p>Easily build cloudevents to inject into AWS Lambda or your local develompent environment</p>
+			</header>
+
+			<h2>AWS Lambda Test Console</h2>
+			<pre>{{ cloudevent }}</pre>
+
+			<div class="mt-3"/>
+			<h2>Local Development Console</h2>
+			<div>'{{ JSON.stringify(cloudevent) }}'</div>
+		</article>
+	</main>
 </template>
