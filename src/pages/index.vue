36270655fa5d199324147f98eda2913d446c4cea
1Mill <erik.ekberg@outlook.com>
2022-09-16 21:46:10 -0700

    Formatting

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,15 @@
 <script>
-import VueJsonEditor from 'vue-json-editor'
-import { Cloudevent } from '@1mill/cloudevents'
+import VueJsonEditor from "vue-json-editor";
+import { Cloudevent } from "@1mill/cloudevents";
 
 export default {
 	head: () => ({
 		link: [
-			{ hid: 'canonical', rel: 'canonical', href: 'https://build.cloudevents.gdn' },
+			{
+				hid: "canonical",
+				rel: "canonical",
+				href: "https://build.cloudevents.gdn",
+			},
 		],
 	}),
 	components: {
@@ -13,50 +17,55 @@ export default {
 	},
 	data: () => ({
 		data: null,
-		source: 'build.cloudevents.gdn',
-		type: 'cmd.placeholder.v0',
+		source: "build.cloudevents.gdn",
+		type: "cmd.placeholder.v0",
 	}),
 	computed: {
 		cloudevent() {
-			if (!this.source) return 'Source is required'
-			if (!this.type) return 'Type is required'
+			if (!this.source) return "Source is required";
+			if (!this.type) return "Type is required";
 
 			const attributes = {
 				source: this.source,
 				type: this.type,
-			}
-			if (this.data) attributes.data = JSON.stringify(this.data)
+			};
+			if (this.data)
+				attributes.data = JSON.stringify(this.data);
 
-			const cloudevent = new Cloudevent(attributes)
-			return cloudevent
+			const cloudevent = new Cloudevent(attributes);
+			return cloudevent;
 		},
 	},
-}
+};
 </script>
 <template>
 	<main>
 		<article>
 			<header>
 				<h1>Cloudevent Builder</h1>
-				<p>Easily build cloudevents to inject into AWS Lambda or your local develompent environment</p>
+				<p>
+					Easily build cloudevents to inject into
+					AWS Lambda or your local develompent
+					environment
+				</p>
 			</header>
 
-			<div class="mt-3"/>
-			<v-text-field v-model="source" label="Source"/>
-			<v-text-field v-model="type" label="Type"/>
+			<div class="mt-3" />
+			<v-text-field v-model="source" label="Source" />
+			<v-text-field v-model="type" label="Type" />
 
-			<div class="mt-3"/>
+			<div class="mt-3" />
 			<vue-json-editor
-			expanded-on-start
-			mode="code"
-			v-model="data"
+				expanded-on-start
+				mode="code"
+				v-model="data"
 			/>
 
-			<div class="mt-3"/>
+			<div class="mt-3" />
 			<h2>AWS Lambda Test Console</h2>
 			<pre>{{ cloudevent }}</pre>
 
-			<div class="mt-3"/>
+			<div class="mt-3" />
 			<h2>Local Development Console</h2>
 			<div>'{{ JSON.stringify(cloudevent) }}'</div>
 		</article>
