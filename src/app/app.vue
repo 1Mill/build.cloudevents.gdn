612d0bf4799aae58ed272bbe4e367be0efd63be6
1Mill <1Mill@users.noreply.github.com>
2025-12-28 14:20:41 -0800

    Scaffold app

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<VApp>
+		<VContainer>
+			<NuxtPage/>
+		</VContainer>
+	</VApp>
 </template>
---
+++ src/app/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	const source = ref("build.cloudevents.gdn")
+	const type = ref("cmd.placeholder.v0")
+	const wschannelid = ref("my-private-channel-name")
+</script>
+
+<template>
+	<VMain>
+		<article>
+			<header>
+				<h1>Cloudevent Builder</h1>
+				<p>Easily build cloudevents to inject into AWS Lambda or your local develompent environment.</p>
+			</header>
+			<section>
+				<VTextField label="Source" v-model="source"/>
+				<VTextField label="Type" v-model="type"/>
+				<VTextField label="WEbsockets Channel ID" v-model="wschannelid"/>
+			</section>
+		</article>
+	</VMain>
+</template>
